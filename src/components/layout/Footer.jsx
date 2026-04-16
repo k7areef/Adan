2d481278818952cef50d38f8c387c45d45a36135
k7areef <kh3reef@gmail.com>
2026-04-16 11:22:11 +0200

    fix(footer): update footer text to include copyright notice with current year

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="bg-primary text-white py-5">
             <div className="container">
-                الزيل
+                <p className="font-semibold text-center">كل الحقوق محفوظة - Adan - {new Date().getFullYear()}</p>
             </div>
         </footer>
     )
