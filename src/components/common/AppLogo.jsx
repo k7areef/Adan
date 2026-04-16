edf9c54ffbc1e46075e97f5c49380c27e7cdda2e
k7areef <kh3reef@gmail.com>
2026-04-16 08:35:04 +0200

    feat(layout): add search input and app logo to header

---
+++ src/components/common/AppLogo.jsx
@@ -0,0 +1,17 @@
+import { faMosque } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+function AppLogo() {
+    return (
+        <Link
+            to={'/'}
+            className="text-2xl font-semibold uppercase flex items-center gap-2"
+        >
+            <span>Adan</span>
+            <FontAwesomeIcon icon={faMosque} className="text-secondary" />
+        </Link>
+    )
+}
+
+export default AppLogo;
